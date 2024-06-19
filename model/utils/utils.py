06dd9a497b95c6e13cd94889a373b3e8c47d768a
Julisa Bana Abraham <bana.abraham@gmail.com>
2024-06-19 17:52:30 +0700

    update the model

--- model/utils/utils.py
+++ model/utils/utils.py
@@ -8,15 +8,17 @@ kmeans = pickle.load(open("pickles/kmeans.pkl", 'rb'))
 # Get kmeans label
 def kmeans_predict(df):
     feature_list = ['Sex_en', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'has_cabin']
-    return kmeans.predict(df[feature_list])
+    return kmeans.predict(df[feature_list].values)
 
-# preprocess
+
 def preprocess(input_dict):
     
+    # Processing Features
     if input_dict['Cabin']:
         has_cabin = 1
     else:
         has_cabin = 0
+        
     if input_dict['Sex'] == 'male':
         Sex_en = 0
     else:
@@ -37,11 +39,21 @@ def preprocess(input_dict):
         'has_cabin':  [has_cabin],
         'Title':  [title],
         'Sex_en':  [Sex_en],
+        'Cabin': [input_dict['Cabin']],
+        'Ticket':[input_dict['Ticket']]
+        
     })
 
+    # Adding additional features
+    df['family_number'] = df.eval("SibSp + Parch")
+    df['is_alone'] = df.eval("family_number < 1")
+    df['cabin_type'] = pd.Categorical(df.Cabin.fillna("-").apply(lambda x: x[0]))
+    df['ticket_type'] = pd.Categorical(df.Ticket.apply(lambda x: x[:3]))
     df['kmeans_label'] = kmeans_predict(df)
     df['Embarked'] = pd.Categorical(df['Embarked'])
     df['Title'] = pd.Categorical(df['Title'])
+
+    df.drop(['Cabin', 'Ticket'], axis=1, inplace=True)
 
     return df
 
